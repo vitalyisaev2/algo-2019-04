b5ff3d4879e216482fcdd21893fcefc1c58720ae
Vitaly Isaev <vitalyisaev2@gmail.com>
2019-05-03 00:23:13 +0300

    ram sorting implemented

--- 01/sort.py
+++ 01/sort.py
@@ -9,18 +9,15 @@ def main():
         array.append(sys.argv[r+2])
         r += 1
 
-    print(array)
     i = 0
     k = 0
     while i < n:
         k = i
         while k < n:
-            print(i, k, array[i], array[k])
             if array[i] > array[k]:
                 array[i], array[k] = array[k], array[i]
             k += 1
         i += 1
-        print(array)
 
     print(array)
 
