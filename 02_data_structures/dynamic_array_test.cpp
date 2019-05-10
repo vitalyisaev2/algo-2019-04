27b9583bdc4df435e29bbbd50995f56c2a981d26
Vitaly Isaev <vitalyisaev2@gmail.com>
2019-05-10 19:51:19 +0300

    template tests for dynamic array

--- 02_data_structures/dynamic_array_test.cpp
+++ 02_data_structures/dynamic_array_test.cpp
@@ -2,62 +2,57 @@
 #include "single_array.hpp"
 #include <catch2/catch.hpp>
 
-TEST_CASE("DynamicArray")
+TEMPLATE_TEST_CASE("DynamicArray operations", "[DynamicArray][template]", (SingleArray<int>))
 {
     SECTION("simple CRUD operations")
     {
-        DynamicArray<int>* array = new SingleArray<int>();
+        TestType array;
 
         // у пустого массива размер равен 0
-        REQUIRE(array->size() == 0);
+        REQUIRE(array.size() == 0);
 
         // записываем элемент в конец
-        array->add(0);
-        REQUIRE(array->size() == 1);
-        REQUIRE(array->get(0) == 0);
+        array.add(0);
+        REQUIRE(array.size() == 1);
+        REQUIRE(array.get(0) == 0);
 
         // удаляем единственный элемент
-        REQUIRE(array->remove(0) == 0);
-        REQUIRE(array->size() == 0);
-
-        delete array;
+        REQUIRE(array.remove(0) == 0);
+        REQUIRE(array.size() == 0);
     }
 
     SECTION("add to front")
     {
-        DynamicArray<int>* array = new SingleArray<int>();
-        array->add(0);
-        array->add(1);
-        array->add(2);
-        array->add(3, 0);
+        TestType array;
+        array.add(0);
+        array.add(1);
+        array.add(2);
+        array.add(3, 0);
 
-        DynamicArray<int>* expected = new SingleArray<int>();
-        expected->add(3);
-        expected->add(0);
-        expected->add(1);
-        expected->add(2);
+        TestType expected;
+        expected.add(3);
+        expected.add(0);
+        expected.add(1);
+        expected.add(2);
 
-        REQUIRE(*array == *expected);
-
-        delete array;
-        delete expected;
+        REQUIRE(array == expected);
     }
 
     SECTION("add to middle")
     {
-        DynamicArray<int>* array = new SingleArray<int>();
-        array->add(0);
-        array->add(1);
-        array->add(2);
-        array->add(3, 2);
+        TestType array;
+        array.add(0);
+        array.add(1);
+        array.add(2);
+        array.add(3, 2);
 
-        DynamicArray<int>* expected = new SingleArray<int>();
-        expected->add(0);
-        expected->add(1);
-        expected->add(3);
-        expected->add(2);
+        TestType expected;
+        expected.add(0);
+        expected.add(1);
+        expected.add(3);
+        expected.add(2);
 
-        REQUIRE(*array == *expected);
+        REQUIRE(array == expected);
 
         delete array;
         delete expected;
@@ -65,19 +60,19 @@ TEST_CASE("DynamicArray")
 
     SECTION("add to back")
     {
-        DynamicArray<int>* array = new SingleArray<int>();
-        array->add(0);
-        array->add(1);
-        array->add(2);
-        array->add(3, 3);
+        TestType array;
+        array.add(0);
+        array.add(1);
+        array.add(2);
+        array.add(3, 3);
 
-        DynamicArray<int>* expected = new SingleArray<int>();
-        expected->add(0);
-        expected->add(1);
-        expected->add(2);
-        expected->add(3);
+        TestType expected;
+        expected.add(0);
+        expected.add(1);
+        expected.add(2);
+        expected.add(3);
 
-        REQUIRE(*array == *expected);
+        REQUIRE(array == expected);
 
         delete array;
         delete expected;
@@ -85,19 +80,19 @@ TEST_CASE("DynamicArray")
 
     SECTION("remove from front")
     {
-        DynamicArray<int>* array = new SingleArray<int>();
-        array->add(0);
-        array->add(1);
-        array->add(2);
-        array->add(3);
-        REQUIRE(array->remove(0) == 0);
+        TestType array;
+        array.add(0);
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        REQUIRE(array.remove(0) == 0);
 
-        DynamicArray<int>* expected = new SingleArray<int>();
-        expected->add(1);
-        expected->add(2);
-        expected->add(3);
+        TestType expected;
+        expected.add(1);
+        expected.add(2);
+        expected.add(3);
 
-        REQUIRE(*array == *expected);
+        REQUIRE(array == expected);
 
         delete array;
         delete expected;
@@ -105,19 +100,19 @@ TEST_CASE("DynamicArray")
 
     SECTION("remove from middle")
     {
-        DynamicArray<int>* array = new SingleArray<int>();
-        array->add(0);
-        array->add(1);
-        array->add(2);
-        array->add(3);
-        REQUIRE(array->remove(2) == 2);
+        TestType array;
+        array.add(0);
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        REQUIRE(array.remove(2) == 2);
 
-        DynamicArray<int>* expected = new SingleArray<int>();
-        expected->add(0);
-        expected->add(1);
-        expected->add(3);
+        TestType expected;
+        expected.add(0);
+        expected.add(1);
+        expected.add(3);
 
-        REQUIRE(*array == *expected);
+        REQUIRE(array == expected);
 
         delete array;
         delete expected;
@@ -125,19 +120,19 @@ TEST_CASE("DynamicArray")
 
     SECTION("remove from back")
     {
-        DynamicArray<int>* array = new SingleArray<int>();
-        array->add(0);
-        array->add(1);
-        array->add(2);
-        array->add(3);
-        REQUIRE(array->remove(3) == 3);
+        TestType array;
+        array.add(0);
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        REQUIRE(array.remove(3) == 3);
 
-        DynamicArray<int>* expected = new SingleArray<int>();
-        expected->add(0);
-        expected->add(1);
-        expected->add(2);
+        TestType expected;
+        expected.add(0);
+        expected.add(1);
+        expected.add(2);
 
-        REQUIRE(*array == *expected);
+        REQUIRE(array == expected);
 
         delete array;
         delete expected;
