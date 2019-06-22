d22302d897470ba91b2df5540af2745475573288
Vitaly Isaev <vitalyisaev2@gmail.com>
2019-06-22 21:28:15 +0300

    HW08 | trie insert/exists

--- 08_sort/trie.hpp
+++ 08_sort/trie.hpp
@@ -3,41 +3,74 @@
 
 #include <iterator>
 #include <map>
+#include <memory>
 
 namespace algo
 {
-    template <class Iterator, class T = typename std::iterator_traits<Iterator>::value_type>
+    // Префиксное дерево, которое работает с последовательностями значений какого-то типа,
+    // которые передаются через итераторы
+    template <typename Iterator, typename T = Iterator::value_type>
     class Trie
     {
       public:
+        Trie() : terminated(false){};
+
         void Insert(Iterator begin, Iterator end)
         {
             if (begin == end) {
+                if (terminated) {
+                    throw "Sequence already exists";
+                }
+                terminated = true;
                 return;
             }
-            T value = *begin;
+            auto value = *begin;
             if (!children.contains(value)) {
                 children[value] = std::make_unique<Trie>();
             }
             children[value]->Insert(++begin, end);
         }
 
-        bool Exists(const Iterator begin, const Iterator end) const
+        bool Exists(Iterator begin, Iterator end)
         {
             if (begin == end) {
-                return true;
+                return terminated;
             }
-            T value = *begin;
+            auto value = *begin;
             if (!children.contains(value)) {
                 return false;
             }
             return children[value]->Exists(++begin, end);
         }
 
-        // void Remove(Iterator begin, Iterator end) {}
+        void Remove(Iterator begin, Iterator end)
+        {
+            if (begin == end) {
+                if (!terminated) {
+                    throw "Sequence does not exist";
+                }
+                terminated = false;
+                return;
+            }
+
+            // если дочернего узла не существует, значит, такой последовательности нет
+            auto value = *begin;
+            if (!children.contains(value)) {
+                throw "Sequence does not exist";
+            }
+
+            // если у дочернего узла нет своих детей, значит, узел удаляется
+            auto child = children[value];
+            if (std::distance(begin, end) == 1 && children->children.size() == 0) {
+                children.erase(value);
+            }
+
+            return child.Remove(++begin, end);
+        }
 
       private:
         std::map<T, std::unique_ptr<Trie>> children;
+        bool                               terminated;
     };
 
 } // namespace algo
--- test/trie_test.cpp
+++ test/trie_test.cpp
@@ -1,11 +1,41 @@
+#include "trie.hpp"
 #include <catch2/catch.hpp>
 #include <vector>
-#include "trie.hpp"
 
-TEST_CASE("Trie") {
-    SECTION("Basic operations") {
-        algo::Trie<std::vector<int>>::iterator> t;
-        std::vector<int> v({1,2,3,4,5});
-        t.Insert(v.begin(), v.end());
+TEST_CASE("Trie")
+{
+    SECTION("Insert")
+    {
+        algo::Trie<std::vector<int>::iterator> trie;
+
+        // вставляем пару последовтельностей
+        std::vector<int> v1({1, 2, 3, 4, 5});
+        trie.Insert(v1.begin(), v1.end());
+        std::vector<int> v2({1, 2, 3, 5, 4});
+        trie.Insert(v2.begin(), v2.end());
+
+        // проверяем, что они имеются в дереве
+        REQUIRE(trie.Exists(v1.begin(), v1.end()));
+        REQUIRE(trie.Exists(v2.begin(), v2.end()));
     }
-};
+
+    SECTION("Insert the same sequence twice")
+    {
+
+        algo::Trie<std::vector<int>::iterator> trie;
+
+        // вставляем пару последовтельностей
+        std::vector<int> v1({1, 2, 3, 4, 5});
+        trie.Insert(v1.begin(), v1.end());
+        REQUIRE_THROWS(trie.Insert(v1.begin(), v1.end()));
+    }
+
+    SECTION("Check non existing sequence")
+    {
+        algo::Trie<std::vector<int>::iterator> trie;
+
+        // любой произвольной последовательности не имеется
+        std::vector<int> v3({0, 0, 0, 0, 0});
+        REQUIRE(!trie.Exists(v3.begin(), v3.end()));
+    }
+}
